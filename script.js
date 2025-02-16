ded983a7f49fbd4ce8d867fc597d88a5793123cc
soylltari <mmmsribranets@gmail.com>
2025-02-16 12:57:05 +0200

    Update README and enhance UI/UX

--- script.js
+++ script.js
@@ -1,113 +1,113 @@
 const letters = {
-    q: "й",
-    w: "ц",
-    e: "у",
-    r: "к",
-    t: "е",
-    y: "н",
-    u: "г",
-    i: "ш",
-    o: "щ",
-    p: "з",
-    "[": "х",
-    "]": "ї",
-    a: "ф",
-    s: "і",
-    d: "в",
-    f: "а",
-    g: "п",
-    h: "р",
-    j: "о",
-    k: "л",
-    l: "д",
-    ";": "ж",
-    "'": "є",
-    z: "я",
-    x: "ч",
-    c: "с",
-    v: "м",
-    b: "и",
-    n: "т",
-    m: "ь",
-    ",": "б",
-    ".": "ю",
-    "?": ",",
-    "/": ".",
+  q: "й",
+  w: "ц",
+  e: "у",
+  r: "к",
+  t: "е",
+  y: "н",
+  u: "г",
+  i: "ш",
+  o: "щ",
+  p: "з",
+  "[": "х",
+  "]": "ї",
+  a: "ф",
+  s: "і",
+  d: "в",
+  f: "а",
+  g: "п",
+  h: "р",
+  j: "о",
+  k: "л",
+  l: "д",
+  ";": "ж",
+  "'": "є",
+  z: "я",
+  x: "ч",
+  c: "с",
+  v: "м",
+  b: "и",
+  n: "т",
+  m: "ь",
+  ",": "б",
+  ".": "ю",
+  "?": ",",
+  "/": ".",
 };
 
 // function for adding exceptions to objects(in case there are more languages in the future)
-function addException(object, key, value){
-    return object[key] = value;
+function addException(object, key, value) {
+  return (object[key] = value);
 }
 
 //create object with upper-case elements
 const capitalLetters = Object.fromEntries(
-    Object.entries(letters).map(([key, value]) => [
-        key.toUpperCase(),
-        value.toUpperCase()
-    ])
+  Object.entries(letters).map(([key, value]) => [
+    key.toUpperCase(),
+    value.toUpperCase(),
+  ])
 );
 
 addException(capitalLetters, "<", "Б");
 addException(capitalLetters, ">", "Ю");
 addException(capitalLetters, "{", "Х");
 addException(capitalLetters, "}", "Ї");
-addException(capitalLetters, ':', "Ж");
+addException(capitalLetters, ":", "Ж");
 addException(capitalLetters, '"', "Є");
 
 //create objects with swapped keys and values
 const swappedLetters = Object.fromEntries(
-    Object.entries(letters).map(([key, value]) => [value, key])
+  Object.entries(letters).map(([key, value]) => [value, key])
 );
 const swappedCapitalLetters = Object.fromEntries(
-    Object.entries(capitalLetters).map(([key, value]) => [value, key])
+  Object.entries(capitalLetters).map(([key, value]) => [value, key])
 );
 
 //merge objects for better iteration
-const initialObjects = {...capitalLetters, ...letters};
-const swappedObjects = {...swappedCapitalLetters, ...swappedLetters};
+const initialObjects = { ...capitalLetters, ...letters };
+const swappedObjects = { ...swappedCapitalLetters, ...swappedLetters };
 //for outputting the result
-const output = document.querySelector(".out");
+const inputText = document.querySelector("#textarea");
+const outputText = document.querySelector(".output");
 
-function converter(){
-    const input = document.getElementById("text").value;
-    let outputArray = [];
-    for (const item of input) {
-        //search for a match in input and objects
-        if (Object.hasOwn(initialObjects, item)) { 
-            outputArray.push(initialObjects[item]);
-        }
-        else if(Object.hasOwn(swappedObjects, item)){
-            outputArray.push(swappedObjects[item]);
-        }
-        else {
-            outputArray.push(item);
-        }
+function converter() {
+  const input = inputText.value;
+  outputText.value = "";
+  let outputArray = [];
+  for (const item of input) {
+    //search for a match in input and objects
+    if (Object.hasOwn(initialObjects, item)) {
+      outputArray.push(initialObjects[item]);
+    } else if (Object.hasOwn(swappedObjects, item)) {
+      outputArray.push(swappedObjects[item]);
+    } else {
+      outputArray.push(item);
     }
-    output.innerHTML = outputArray.join("");
+  }
+  outputText.value = outputArray.join("");
 }
 
 //function to clear input
-function clear(){
-    document.getElementById("text").value = "";
-    output.innerHTML = "";
+function clear() {
+  inputText.value = "";
+  outputText.value = "";
 }
 
 //function to copy the result
-function copy(){
-    const copyInput = document.querySelector(".out").innerHTML;
-   if(copyInput){
-        navigator.clipboard.writeText(copyInput);
-        copyHint();
-    }
+function copy() {
+  const copyInput = outputText.value;
+  if (copyInput) {
+    navigator.clipboard.writeText(copyInput);
+    copyHint();
+  }
 }
 //function that appears when the result is copied
-function copyHint(){
-    const hint = document.querySelector(".hint");
-    hint.classList.add("show-hint");
-    setTimeout( function(){
-        hint.classList.remove("show-hint");
-    }, 1200);
+function copyHint() {
+  const hint = document.querySelector(".hint");
+  hint.classList.add("show-hint");
+  setTimeout(function () {
+    hint.classList.remove("show-hint");
+  }, 1200);
 }
 
 document.querySelector(".b-convert").addEventListener("click", converter);
